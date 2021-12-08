a1714adf6f2645736e4056fd432853ca570d69ac
marco <marco.paulo.rm@gmail.com>
2021-12-08 14:49:39 -0300

    change: for deploy without database

--- scriptDocker.sh
+++ scriptDocker.sh
@@ -3,7 +3,7 @@
 dotnet tool install --global dotnet-ef --version 5.0.12
 export PATH="$PATH:/root/.dotnet/tools"
 cd /home/full-backend
-dotnet ef database update
+#dotnet ef database update
 dotnet publish -c Release
 mkdir /home/backend
 cp -r bin/Release/net5.0/publish/* /home/backend
