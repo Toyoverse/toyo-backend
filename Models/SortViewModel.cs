d85fa1255b7690278e81cc502b444cf4fe1235b9
marco <marco.paulo.rm@gmail.com>
2021-12-09 18:30:26 -0300

    fix

--- Models/SortViewModel.cs
+++ Models/SortViewModel.cs
@@ -3,7 +3,7 @@ namespace BackendToyo.Models
     public class SortViewModel
     {
         public int[][] oqParts  { get; set; }
-        public int[][] qStats  { get; set; }
+        public int[] qStats  { get; set; }
         public int toyoRaridade { get; set; }
     }
 }
