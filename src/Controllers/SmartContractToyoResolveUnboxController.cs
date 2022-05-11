7e2d8066092b3c48c18d3216b6a130241301f1b8
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2022-05-11 15:27:36 -0300

    Check condition for unbox

--- src/Controllers/SmartContractToyoResolveUnboxController.cs
+++ src/Controllers/SmartContractToyoResolveUnboxController.cs
@@ -4,8 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BackendToyo.Data;
+using BackendToyo.Models;
+using BackendToyo.Models.DataEntities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackendToyo.Controllers
 {
@@ -21,10 +24,21 @@ namespace BackendToyo.Controllers
         }
         //GET: api/SmartContractToyoResolveUnbox
         [HttpGet]
-        [Authorize(Roles = "Block Chain Service")]
-        public async Task<IEnumerable> GetSmartContractToyoResolveUnbox(int tokenId, String walletAdress)
+        //[Authorize(Roles = "Block Chain Service")]
+        public async Task<ActionResult<IEnumerable<SmartContractToyoSwap>>> GetSmartContractToyoResolveUnbox(int tokenId, String walletAdress)
         {
-            return null;
+            var swap = await _context.SmartContractToyoSwaps.Where(p=> p.ToTokenId == tokenId).ToListAsync();
+            if (swap ==null || swap.Count() != 1) return null;
+            var transfer = await _context.SmartContractToyoTransfers.Where(p => p.TokenId == tokenId).ToListAsync();
+            if (transfer ==null || transfer.Count() != 1) return null;
+            var mint = await _context.SmartContractToyoMints.Where(p=> p.TokenId == tokenId).ToListAsync();
+            if (mint == null || mint.Count() != 1) return null;
+            var toyoPlayer = await _context.ToyosPlayer.Where(p=> p.WalletAddress == walletAdress && p.TokenId == tokenId).ToListAsync();
+            if (toyoPlayer == null || toyoPlayer.Count() != 1) return null;
+            //toDo
+            return swap; //Alterar o tipo de retorno ao termino da task
         }
+
+
     }
 }
